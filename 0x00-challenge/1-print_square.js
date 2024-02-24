016711301ce88dfaa2a7d36c6d4959fd7e0bd6e2
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-24 07:00:56 +0200

    “commit”

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
 /*
     Print a square with the character #
     
@@ -16,8 +16,8 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2])
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0 ; i < size ; i++) {
+    for (let j = 0 ; j < size ; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
